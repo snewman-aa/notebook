f854ec74bc151c44eeab6021560770ea1f2b3773
snewman <snewman.aa@gmail.com>
2025-02-11 12:07:34 -0500

    Add docs

--- api.py
+++ api.py
@@ -8,6 +8,7 @@ notebook = NoteBook()
 
 @api.get("/")
 def read_root():
+    """Returns the instructions for the API"""
     message = ("/static/instructions.html for the main page"
                " /list for the list of notes"
                " /write?name=NAME&content=CONTENT to add a note"
@@ -18,11 +19,18 @@ def read_root():
 
 @api.get("/notes")
 def list_notes():
-    return {"notes": note.name for note in notebook.list_notes()}
+    """Lists all the notes in the notebook"""
+    return {"notes": [note for note in notebook.list_notes()]}
+
+@api.get("/list")
+def list_note_names():
+    """Lists all the names of the notes in the notebook"""
+    return {"notes": [note.name for note in notebook.list_notes()]}
 
 
 @api.get("/note/{name}")
 def read(name: str):
+    """Reads a note from the notebook"""
     note = notebook.read(name)
     if note:
         return {"note": note}
@@ -31,21 +39,33 @@ def read(name: str):
 
 @api.delete("/note")
 def remove(name: str):
+    """Removes a note from the notebook"""
     notebook.remove(name)
-    return {"message": f"Note {name} removed successfully"}
+    return {"message": f'Note "{name}" removed successfully'}
 
 
 @api.post("/add")
 def write(note: Note):
+    """
+    Adds a Note to the notebook
+    Will not allow adding a note with the same name as an existing note
+    :param note:
+    """
     name = note.name
     if notebook.read(name):
-        return {"message": f"Note {name} already exists"}
+        return {"message": f'Note "{name}" already exists'}
     notebook.add_note(note)
     return {"message": f'Note "{name}" written successfully'}
 
 
 @api.get("/find")
 def find(term: str):
+    """
+    Returns the names of all the notes that contain the search term in their
+    content
+    :param term:
+    :return: list of note names
+    """
     notes = notebook.search(term)
     if notes:
         return {"term": term, "notes": notes}
